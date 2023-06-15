3d794a4e45525a8210ed90e6e66f10ed25bece21
Mudi-san <mudijewilly@gmail.com>
2023-06-15 05:53:00 -0700

    cute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char art[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
