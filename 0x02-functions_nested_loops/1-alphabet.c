8cd553590e08293f77b0dbd2a0a25ee019f312eb
Mudi-san <mudijewilly@gmail.com>
2023-06-20 07:31:10 -0700

    okay

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * print_alphabet - print alphabets.
- * 
+ * print_alphabet - check the code for ALX School students.
+ *
  * Return: Always 0.
  */
-
 void print_alphabet(void)
 {
 	char alph = 'a';
